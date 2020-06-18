a622ffbbf619324598d631580cdd1193642efe12
mgmike1011 <60261042+mgmike1011@users.noreply.github.com>
2020-06-18 18:53:25 +0200

    18:53/18.06
    Poprawiona i wyczyszczona klasa Enemy.

--- Projekt_Gra/enemy.cpp
+++ Projekt_Gra/enemy.cpp
@@ -1,8 +1,8 @@
 #include "enemy.h"
 
-void Enemy::setTexture(sf::Texture tekstura)
+void Enemy::setTexture(sf::Texture tekstura_)
 {
-    tekstury.emplace_back(tekstura);
+    Tekstura = tekstura_;
 }
 
 bool Enemy::getifkill()
@@ -25,76 +25,25 @@ void Enemy::setkill(bool statement)
     kill_c = statement;
 }
 
-std::vector<sf::Vector2f> Enemy::getwierzcholki()
+Enemy::Enemy(sf::Vector2f &pozycja)
 {
-    return wierzcholki;
-}
-
-Enemy::Enemy() : sf::ConvexShape()
-{
-    speed_c = rand()%5+1;
-    kierunek_c= 360/(rand()%6+1);
-    setPosition(1333,rand()%720+10);
-    sf::Texture tx;
-    if(!tx.loadFromFile("resources/Przeciwnik/playerShip3_blue.png"))
+    int zmienna = rand()%10+2;
+    punkty_hpMax = zmienna;
+    typ =0;
+    punkty_hp = punkty_hpMax;
+    damage_ = zmienna;
+    Punkty_enemy = zmienna;
+    //Ladowanie tekstury
+    if(!Tekstura.loadFromFile("resources/Przeciwnik/playerShip3_blue.png"))
     {
         std::cout<<"Nie udalo sie zaladowac tekstury statku przeciwnika"<<std::endl;
-    }else
-    {
-        tekstury.emplace_back(tx);
     }
-    if(!tx.loadFromFile("resources/Przeciwnik/playerShip3_green.png"))
-    {
-        std::cout<<"Nie udalo sie zaladowac tekstury statku przeciwnika"<<std::endl;
-    }else
-    {
-        tekstury.emplace_back(tx);
-    }
-    if(!tx.loadFromFile("resources/Przeciwnik/playerShip3_orange.png"))
-    {
-        std::cout<<"Nie udalo sie zaladowac tekstury statku przeciwnika"<<std::endl;
-    }else
-    {
-        tekstury.emplace_back(tx);
-    }
-    int los = rand()%3;
-    switch (los)
-    {
-    case 0 :
-        setTexture(tekstury[0]);
-        rotate(-90);
-        break;
-    case 1 :
-        setTexture(tekstury[1]);
-        rotate(-90);
-        break;
-    case 2 :
-        setTexture(tekstury[2]);
-        rotate(-90);
-        break;
-    }
-    setPoint(0,sf::Vector2f(getPosition().x,getPosition().y+100/speed_c));
-    wierzcholki.emplace_back(getPoint(0));
-    setPoint(1,sf::Vector2f(getPosition().x,getPosition().y+100/speed_c));
-    wierzcholki.emplace_back(getPoint(1));
-
-    setPosition(1500,rand()%720);
+    //Ustawianie tekstury
+    this->setTexture(Tekstura);
+    //Ustwienia figury
+    this->setRadius(25);
+    this->setPointCount(5);
+    this->setPosition(pozycja);
 }
 
-void Enemy::update_enemy()
-{
-    if(hit_c == true)
-    {
-        if(getPointCount()>2)
-        {
-            move(30,0);
-            speed_c=speed_c*1.2;
-            setPointCount(getPointCount()-1);
-            wierzcholki.pop_back();
-        }else{
-            kill_c=true;
-        }
-        hit_c=false;
-    }
-    setPosition(getPosition().x-speed_c,getPosition().y+kierunek_c);
-}
+
